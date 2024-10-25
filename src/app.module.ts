a129e48c09083529c13dcf0bd1a1f2e6d4ece50f
Ichikyube <ichikyube@gmail.com>
2024-10-25 14:56:27 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ArticleModule } from './features/article/article.module';
+import { MediaModule } from './features/media/media.module';
+import { AnalyticsModule } from './features/analytics/analytics.module';
+import { NewsletterModule } from './features/newsletter/newsletter.module';
+
+@Module({
+  imports: [UserModule, ArticleModule, MediaModule, AnalyticsModule, NewsletterModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
